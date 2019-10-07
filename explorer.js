abc1f3679fdaab1b3f5ccb558d0b8f5a9a2d0bc7
Tzvetan Mikov <tmikov@fb.com>
2019-10-07 10:34:31 -0700

    Small improvements

--- explorer.js
+++ explorer.js
@@ -4,39 +4,83 @@ function onLoad() {
     var commandLine = document.getElementById("commandLine");
     var runHermes = document.getElementById("runHermes");
 
+    var output = "";
+    var runtimeInitialized = false;
+    var runRequested = 0;
+    var runCommandLine = "";
+    var app;
+    var lastTime;
+
     runHermes.onclick = onRunHermes;
 
+    startRuntimeInitialization();
+
+    function ellapsed() {
+        if (!lastTime) {
+            lastTime = new Date();
+            return 0 + " ms";
+        }
+        var newTime = new Date();
+        var res = newTime - lastTime;
+        lastTime = newTime;
+        return res + " ms";
+    }
+
+    function startRuntimeInitialization() {
+        console.log(ellapsed(), "startRuntimeInit");
+        runtimeInitialized = false;
+        app = createApp({
+            print: handleStdout, printErr: handleStdout, onRuntimeInitialized: onRuntimeInitialized
+        });
+    }
+
+    function onRuntimeInitialized() {
+        console.log(ellapsed(), "runtime ready");
+        runtimeInitialized = true;
+        if (runRequested)
+            setTimeout(executeHermes, 0);
+    }
+
     function onRunHermes() {
+        console.log(ellapsed(), "run requested");
+        if (runRequested)
+            return;
+        outputArea.innerHTML = "Running...";
+        runCommandLine = commandLine.value;
+        runRequested = 1;
+
+        if (runtimeInitialized)
+            setTimeout(executeHermes, 0);
+    }
+
+    function executeHermes() {
+        console.log(ellapsed(), "executing hermes");
+        runRequested = 2;
+
+        var args = commandLine.value.split(/\s+/);
+
+        var fileName = "/tmp/hermes-input.js";
+        app.FS.writeFile(fileName, inputArea.value);
+
+        args.push("--pretty-json");
+        args.push(fileName);
+
+        app.callMain(args);
+        console.log(ellapsed(), "hermes done");
+
         // Delete all child nodes.
         outputArea.innerHTML = "";
-        var output = "";
+        var pre = document.createElement("pre");
+        pre.innerText = output;
+        outputArea.appendChild(pre);
 
-        function handleStdout(txt) {
-            output += txt;
-            output += "\n";
-            // outputArea.appendChild(document.createTextNode(txt));
-            // outputArea.appendChild(document.createElement("br"));
-        }
+        output = "";
+        runRequested = 0;
+        startRuntimeInitialization();
+    }
 
-        var app = createApp({
-            print: handleStdout, printErr: handleStdout, onRuntimeInitialized: function () {
-
-                var args = commandLine.value.split(/\s+/);
-                if (args.length === 0) {
-                    args.push("--help");
-                } else {
-                    var fileName = "/tmp/hermes-input.js";
-                    app.FS.writeFile(fileName, inputArea.value);
-                    args.push("--pretty-json");
-                    args.push(fileName);
-                }
-
-                app.callMain(args);
-
-                var pre = document.createElement("pre");
-                pre.innerText = output;
-                outputArea.appendChild(pre);
-            }
-        });
+    function handleStdout(txt) {
+        output += txt;
+        output += "\n";
     }
 }
