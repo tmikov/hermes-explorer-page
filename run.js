385b6126e6bcdbefbafd2ba10671740498122360
Tzvetan Mikov <tmikov@fb.com>
2019-10-08 23:29:29 -0700

    delete extra files

--- run.js
+++
@@ -1,9 +0,0 @@
-const hello = require("./hermes.js");
-var prog = hello({print: myprint, printErr: myprint});
-
-prog.FS.writeFile("/tmp/hermes-input.txt", "This is data in a file");
-prog.callMain(["--help"]);
-
-function myprint(str) {
-    console.log("MY", str);
-}
