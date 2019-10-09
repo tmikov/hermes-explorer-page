e70d14ecbdf4f6c7df3e60699e39c93875d06eca
Tzvetan Mikov <tmikov@fb.com>
2019-10-08 19:21:31 -0700

    Async worker and better arg splitting
    Move Hermes into a worker and run it asynchronously.
    Improve argument splitting.

--- explorer.js
+++ explorer.js
@@ -3,80 +3,25 @@ function onLoad() {
     var commandLine = document.getElementById("commandLine");
     var runHermes = document.getElementById("runHermes");
 
-    var output = "";
-    var runtimeInitialized = false;
-    var runRequested = 0;
-    var runCommandLine = "";
-    var app;
-    var lastTime;
-
-    runHermes.onclick = onRunHermes;
-
-    startRuntimeInitialization();
-
-    function ellapsed() {
-        if (!lastTime) {
-            lastTime = new Date();
-            return 0 + " ms";
-        }
-        var newTime = new Date();
-        var res = newTime - lastTime;
-        lastTime = newTime;
-        return res + " ms";
-    }
-
-    function startRuntimeInitialization() {
-        console.log(ellapsed(), "startRuntimeInit");
-        runtimeInitialized = false;
-        app = createApp({
-            print: handleStdout, printErr: handleStdout, onRuntimeInitialized: onRuntimeInitialized
-        });
-    }
-
-    function onRuntimeInitialized() {
-        console.log(ellapsed(), "runtime ready");
-        runtimeInitialized = true;
-        if (runRequested)
-            setTimeout(executeHermes, 0);
-    }
-
-    function onRunHermes() {
-        console.log(ellapsed(), "run requested");
+    var runRequested = false;
+    runHermes.onclick = function () {
         if (runRequested)
             return;
         outputArea.innerText = "Running...";
-        runCommandLine = commandLine.value;
         runRequested = 1;
 
-        if (runtimeInitialized)
-            setTimeout(executeHermes, 0);
-    }
+        var args = commandLine.value.split(/\s+/).filter(x => x);
 
-    function executeHermes() {
-        console.log(ellapsed(), "executing hermes");
-        runRequested = 2;
+        worker.postMessage(["run", args, editor.getValue()])
+    };
 
-        var args = commandLine.value.split(/\s+/);
-
-        var fileName = "/tmp/hermes-input.js";
-        app.FS.writeFile(fileName, editor.getValue());
-
-        args.push("--pretty-json");
-        args.push(fileName);
-
-        app.callMain(args);
-        console.log(ellapsed(), "hermes done");
-
-        // Delete all child nodes.
-        outputArea.innerText = output;
-
-        output = "";
-        runRequested = 0;
-        startRuntimeInitialization();
-    }
-
-    function handleStdout(txt) {
-        output += txt;
-        output += "\n";
-    }
+    var worker = new Worker("worker.js");
+    worker.onmessage = function (e) {
+        switch (e.data[0]) {
+            case "runResult":
+                runRequested = false;
+                outputArea.innerText = e.data[1];
+                break;
+        }
+    };
 }
